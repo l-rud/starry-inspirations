4c4587f0cafdd8a6cefaa8bf7b38ed3e9130acad
Larysa Rud <larpavrud@gmail.com>
2024-06-18 09:58:54 -0400

    Creating HomePage and Layout components

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,8 @@
 import { useState } from 'react'
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Layout from "./pages/Layout";
+import HomePage from "./pages/HomePage";
+
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
@@ -7,25 +11,13 @@ function App() {
    const [count, setCount] = useState(0)
 
   return (
-    <>
-      <div class="container">
-    
-        <div class="item">Aries</div>
-        <div class="item">Taurus</div>
-        <div class="item">Gemini</div>
-        <div class="item">Cancer</div>
-        <div class="item">Leo</div>
-        <div class="item">Virgo</div>
-
-
-        <div class="item">Libra</div>
-        <div class="item">Scorpio</div>
-        <div class="item">Sagittarius</div>
-        <div class="item">Capricornus</div>
-        <div class="item">Aquarius</div>
-        <div class="item">Pisces</div>
-      </div>
-    </>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<HomePage />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
   )
 }
 
---
+++ src/pages/HomePage.jsx
@@ -0,0 +1,22 @@
+function HomePage() {
+  return (
+    <div className="container">
+      <div className="item">Aries</div>
+      <div className="item">Taurus</div>
+      <div className="item">Gemini</div>
+      <div className="item">Cancer</div>
+      <div className="item">Leo</div>
+      <div className="item">Virgo</div>
+
+
+      <div className="item">Libra</div>
+      <div className="item">Scorpio</div>
+      <div className="item">Sagittarius</div>
+      <div className="item">Capricornus</div>
+      <div className="item">Aquarius</div>
+      <div className="item">Pisces</div>
+      </div>
+  );
+}
+
+export default HomePage;
